2a5e44b26929b22581f03b5cc62c31ee452bda73
Natalia <natalia_terekhova@yahoo.com>
2023-01-26 08:48:46 +0300

    edited example 012 methods

--- Examples/Example012_Methods/Program.cs
+++ Examples/Example012_Methods/Program.cs
@@ -27,17 +27,42 @@ int year = Method3();
 
 // Вид 4
 
+// string Method4(int count, string text)
+// {
+// int i=0;
+// string result =String.Empty;
+
+// while (i<count)
+// {
+//     result=result+text;
+//     i++;
+// }
+// return result;
+// }
+// string res = Method4 (10, "asdf");
+// System.Console.WriteLine(res);
+
 string Method4(int count, string text)
 {
-int i=0;
 string result =String.Empty;
 
-while (i<count)
+for (int i=0; i<count; i++)
 {
-    result=result+text;
-    i++;
+    result=result+text; 
 }
 return result;
 }
 string res = Method4 (10, "asdf");
-System.Console.WriteLine(res);
+// System.Console.WriteLine(res);
+
+
+//Вывести на экран таблицу умножения
+
+for (int i=2; i<=10; i++)
+{
+    for (int j=2; j<=10; j++)
+    {
+        System.Console.WriteLine( $"{i}x{j}={i*j}");
+    }
+    System.Console.WriteLine();
+}
